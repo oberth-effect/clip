f0825537d6aa43a3ca3f01bb4c7054f0e8c10806
oberth-effect <stepan.venclik@gmail.com>
2024-08-20 21:21:28 +0200

    add all systems

--- flake.nix
+++ flake.nix
@@ -8,9 +8,9 @@
     let
       supportedSystems = [
         "x86_64-linux"
-        # "aarch64-linux"
-        # "x86_64-darwin"
-        # "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
       forEachSupportedSystem =
         f:
